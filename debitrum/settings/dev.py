956eadc4a50258a9528f8a8538b2d5f989d33668
George Ramaniuk <george.ramaniuk@gmail.com>
2018-10-19 17:17:44 +0300

    Init

---
+++ debitrum/settings/dev.py
@@ -0,0 +1,9 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'xit(-1e(53z+_mr=un&=!clrn-bps7+&2__41a4!1cefnc^zs@'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
